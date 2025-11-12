e3662f78dd3317d3d9ed67563d266725ac2b391e
mtaha44 <mt4458649@gmai.com>
2025-11-12 17:03:15 +0500

    old version harhat + facing issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.28",
-  networks : {
-    sepolia:{
+  networks: {
+    sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
-      accounts:[process.env.SEPOLIA_RPC_PRIVATE_KEYS]
-    }
-  }
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,36 @@
-const { ethers } = require("hardhat");
+const { ethers , run , network } = require("hardhat");
 
 async function DeployContract() {
-    const SimpleStorage = ethers.getContractFactory(
+    const SimpleStorage = await ethers.getContractFactory(
         "SimpleStorage"
     )
 
     console.log("Contract Deploying ... Wait!!")
-    const contractdeploy = SimpleStorage.deploy();
-    await contractdeploy.deployed();
-    console.log("Contract deployed at:", contractdeploy.address);
+    const contractdeploy = await SimpleStorage.deploy();
+    await contractdeploy.waitForDeployment();
+    const contractAddress = await contractdeploy.getAddress();
+    console.log("Contract deployed at:", contractAddress);
+    console.log(network.config);
+    // await verify(contractAddress,"");
+}
+
+async function verify( contractAddress,args) {
+    console.log("Verifying Contract");
+
+    try{
+        await run("verify:verify", {
+          address: contractAddress,
+          constructorArguments: args,
+        });
+        console.log("Contract verifed Successfully");
+    }catch(error){
+        if(error.message.toLowerCase().includes("already verified")){
+            console.log("contract already verified");
+        }
+        else{
+            console.log(error);
+        }
+    }
 }
 
 DeployContract()
